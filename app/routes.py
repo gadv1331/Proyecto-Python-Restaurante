16ade17ea70a5a7aec12bb4153a706673dc6e98f
gadv1331 <gadv1331@gmail.com>
2024-06-25 22:12:00 -0400

    Se agrego la funcionalidad para verificar el rol del usuario

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,9 @@ from infrastructure.db.database import get_db
 from infrastructure.db.repositories.user_repository import UserRepository
 from domain.schemas.user import User as UserSchema
 from infrastructure.security.oauth2 import get_current_active_user
+from infrastructure.security.oauth2 import get_current_chef_user
+from infrastructure.security.oauth2 import get_current_camarero_user
+from infrastructure.security.oauth2 import get_current_cliente_user
 from domain.schemas.user import UserCreate, User
 
 router = APIRouter()
@@ -23,6 +26,7 @@ def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2Passw
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = auth_service.create_access_token_for_user(user)
+    print("token creado:",access_token)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
@@ -31,12 +35,21 @@ def read_users_me(current_user: UserSchema = Depends(get_current_active_user)):
     return current_user
 
 
-@router.post("/", response_model=User)
+@router.post("/registro_usuario", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user_repository = UserRepository(db)
     user_service = UserService(user_repository)
     return user_service.create_user(user)
 
-#@router.get("/me", response_model=User)
-#def read_users_me(current_user: User = Depends(get_current_user)):
-#    return current_user
+@router.get("/chef")
+def get_chef_message(current_user: UserSchema = Depends(get_current_chef_user)):
+    return {"message": "Hola, soy un chef"}
+
+@router.get("/camarero")
+def get_camarero_message(current_user: UserSchema = Depends(get_current_chef_user)):
+    return {"message": "Hola, soy un camarero"}
+
+@router.get("/cliente")
+def get_cliente_message(current_user: UserSchema = Depends(get_current_chef_user)):
+    return {"message": "Hola, soy un cliente"}
+
--- infrastructure/security/oauth2.py
+++ infrastructure/security/oauth2.py
@@ -5,6 +5,7 @@ from infrastructure.security.jwt_handler import SECRET_KEY, ALGORITHM
 from infrastructure.db.database import get_db
 from sqlalchemy.orm import Session
 from infrastructure.db.schemas_orm.user import User as UserModel
+from domain.schemas.user import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/token")
 
@@ -14,6 +15,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -26,12 +28,29 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
     return user
 
+
+
 def get_current_active_user(current_user: UserModel = Depends(get_current_user)):
     #if not current_user.is_active:
     #    raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-def get_current_admin_user(current_user: UserModel = Depends(get_current_active_user)):
+def get_current_chef_user(current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "chef":
+        raise HTTPException(status_code=403, detail="User without the necessary permissions")
+    return current_user
+
+def get_current_camarero_user(current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "camarero":
+        raise HTTPException(status_code=403, detail="User without the necessary permissions")
+    return current_user
+
+def get_current_cliente_user(current_user: User = Depends(get_current_active_user)):
+    if current_user.role != "cliente":
+        raise HTTPException(status_code=403, detail="User without the necessary permissions")
+    return current_user
+
+def get_current_admin_user(current_user: User = Depends(get_current_active_user)):
     if current_user.role != "admin":
         raise HTTPException(status_code=403, detail="Not enough permissions")
     return current_user
