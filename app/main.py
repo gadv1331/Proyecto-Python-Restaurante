b067d469531f9c71ddc5655d295fc3891fba312c
gadv1331 <gadv1331@gmail.com>
2024-07-04 02:41:32 -0400

    Se agrego un titulo y una descripcion al proyecto con swagger

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ from infrastructure.db.database import engine, Base
 # Creacion las tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Restaurante API",
+    version="1.0.0",
+    description="El objetivo de este proyecto es desarrollar el backend de un sistema de gestión para un restaurante. El sistema permitirá a los administradores del restaurante gestionar inventarios de ingredientes, elaborar menús, procesar pedidos de los clientes, y generar reportes diversos.",
+)
 
 #Las rutas de los endpoints
 app.include_router(router, prefix="/api/v1", tags=["router"])
